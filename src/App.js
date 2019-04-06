27e62b3366a9597bebbd702142ab383f205f2de7
kanzariamine <kanzari.amin@gmail.com>
2019-04-06 14:01:32 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,91 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+function Todo ({value, index, completeTodo, deleteTodo}){
+  console.log(index)
+  return(
+    <div 
+    style={{textDecoration: value.isCompleted ? 'line-through' : ''}}
+    className="todo" 
+    key={index}
+    >
+    {value.text}
+     <div>
+      <button onClick={() => completeTodo(index)}> Complete</button>
+      <button onClick={() => deleteTodo(index)}> Delete</button>
+     </div>
+    </div>
+   
+  )
+}
+
+function TodoForm({addTodo}) {
+  const [value, setValue] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
   }
+  return(
+    <form onSubmit={handleSubmit}>
+      <input type="text" className="input" placeholder="Add ToDo..." value={value} onChange={e => setValue(e.target.value)}/>
+    </form>
+  )
+}
+
+function App(){
+  const [todos, setTodos] = useState([
+  {
+    text: 'Learn about React',
+    isCompleted: false
+  },
+  {
+    text: 'Meet friend for lunch',
+    isCompleted: false
+  },
+  {
+    text: 'Build really cool react app',
+    isCompleted: false
+  }
+]);
+
+const addTodo = text => {
+  const NewTodos = [...todos, {text, isCompleted: false}];
+  setTodos(NewTodos);
+}
+
+const completeTodo = index => {
+  
+  const NewTodos = [...todos];
+  console.log('NewTodos',NewTodos[index])
+  NewTodos[index].isCompleted = true;
+  setTodos(NewTodos);
+}
+
+const deleteTodo = index => {
+  const NewTodos = [...todos];
+  NewTodos.splice(index, 1)
+  setTodos(NewTodos);
+}
+return(
+  <div className="app">
+    <div className="todo-list">
+      {
+        todos.map((todo, index) => (
+          <Todo 
+          key={index} 
+          value={todo} 
+          completeTodo={() => completeTodo(index)} 
+          deleteTodo={() => deleteTodo(index)}
+          />
+        ))
+      }
+      <TodoForm addTodo={addTodo}/>
+    </div>
+  </div>
+)
 }
 
 export default App;
