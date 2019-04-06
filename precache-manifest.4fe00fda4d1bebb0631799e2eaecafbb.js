9d7bdc248ab7f29aab7f2ba26500b13593fbc95c
kanzariamine <kanzari.amin@gmail.com>
2019-04-06 14:34:14 +0100

    Updates

---
+++ precache-manifest.4fe00fda4d1bebb0631799e2eaecafbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ded6d438a6d40ecadb29",
+    "url": "/Todo-Hooks/static/js/runtime~main.cf8da72c.js"
+  },
+  {
+    "revision": "084c9e34ea386163dbe1",
+    "url": "/Todo-Hooks/static/js/main.8c06bac1.chunk.js"
+  },
+  {
+    "revision": "af1b6b08e2a705d0b8e7",
+    "url": "/Todo-Hooks/static/js/2.7b0e938e.chunk.js"
+  },
+  {
+    "revision": "084c9e34ea386163dbe1",
+    "url": "/Todo-Hooks/static/css/main.81a5809f.chunk.css"
+  },
+  {
+    "revision": "7e6fd1860c68fb8d4ad7931feab2d103",
+    "url": "/Todo-Hooks/index.html"
+  }
+];
